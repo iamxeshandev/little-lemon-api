8a8aa30856d2de8a91ca06423602bdb116613e3b
Zeshan Mehmood <iamxeshan@gmail.com>
2025-12-28 18:33:08 +0500

    feat: Initialize Django project with DRF, Djoser, and restaurant app settings and URL configurations.

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
     'restaurant',
 ]
 
@@ -117,3 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+}
+
+DJOSER = {
+    'USER_ID_FIELD': 'username',
+}
