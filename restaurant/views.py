1083437075a15c0a021f5f5143896ee9e0f4be5d
Zeshan Mehmood <iamxeshan@gmail.com>
2025-12-28 18:34:27 +0500

    feat: Implement API endpoints for menu items and bookings, and add token authentication.

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
 
@@ -16,6 +17,7 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
     serializer_class = MenuSerializer
 
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
 
